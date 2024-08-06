c21c3bf7d48d329d61045ed563c449b61484a758
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-06 16:59:02 +0900

    고객 컴포넌트 만들기 및 고개 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Customer from "./components/Customer";
 //실질적인 웹사이트 화면에 대한 내용 출력
+
+const customers = [
+  {
+    id: 1,
+    image: "https://i.pravatar.cc/64?img=1",
+    name: "임예진",
+    birthday: "000423",
+    gender: "여자",
+    job: "서포터",
+  },
+  {
+    id: 2,
+    image: "https://i.pravatar.cc/64?img=2",
+    name: "맹일진",
+    birthday: "990525",
+    gender: "남자",
+    job: "정글",
+  },
+  {
+    id: 3,
+    image: "https://i.pravatar.cc/64?img=3",
+    name: "최유한",
+    birthday: "978008",
+    gender: "남자",
+    job: "원딜",
+  },
+];
 function App() {
   return (
-    <div className="gray-background">
-        <img src={logo} lat="logo"/>
-        <h2>메니저 프로그램 시작해보자구!</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+          key={c.id} //중복불가 키값
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
