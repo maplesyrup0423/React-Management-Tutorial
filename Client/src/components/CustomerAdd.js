a70fd4979c7aa75b9e819b5aa0805fd627703cbb
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-07 17:36:48 +0900

    props 써서 고객 정보 갱신

--- Client/src/components/CustomerAdd.js
+++ Client/src/components/CustomerAdd.js
@@ -1,112 +1,124 @@
-import React from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-class CustomerAdd extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      File: null, //바이트 형태의 데이터
-      name: "",
-      birthday: "",
-      gender: "",
-      job: "",
-      fileName: "", //파일이름
-    };
-  }
-  handleFromSubmit = (e) => {
+function CustomerAdd({ stateRefresh }) {
+  const [file, setFile] = useState(null);
+  const [name, setName] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [gender, setGender] = useState('');
+  const [job, setJob] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]); // 하나의 파일만
+    setFileName(e.target.value);
+  };
+
+  const handleValueChange = (e) => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'birthday':
+        setBirthday(value);
+        break;
+      case 'gender':
+        setGender(value);
+        break;
+      case 'job':
+        setJob(value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleFromSubmit = async (e) => {
     e.preventDefault();
-    this.addCustomer().then((response) => {
-      console.log(response.data);
-    });
-    this.setState({
-      File: null,
-      name: "",
-      birthday: "",
-      gender: "",
-      job: "",
-      fileName: "",
-    });
-   window.location.reload();
+    try {
+      await addCustomer();
+      // 상태를 새로 고침하거나, 필요한 경우 새로 고침을 호출합니다.
+      if (stateRefresh) {
+        stateRefresh();
+      }
+      // 상태를 리셋합니다.
+      setFile(null);
+      setName('');
+      setBirthday('');
+      setGender('');
+      setJob('');
+      setFileName('');
+    } catch (error) {
+      console.error('고객 추가 실패:', error);
+    }
   };
 
-  handleFileChange = (e) => {
-    this.setState({
-      file: e.target.files[0], //하나의 파일만
-      fileName: e.target.value,
-    });
-  };
-
-  handleValueChange = (e) => {
-    let nextState = {};
-    nextState[e.target.name] = e.target.value;
-    this.setState(nextState);
-  };
-
-  addCustomer = () => {
+  const addCustomer = () => {
     const url = '/api/customers';
     const formData = new FormData();
-    formData.append("image", this.state.file);
-    formData.append("name", this.state.name);
-    formData.append("birthday", this.state.birthday);
-    formData.append("gender", this.state.gender);
-    formData.append("job", this.state.job);
+    formData.append('image', file);
+    formData.append('name', name);
+    formData.append('birthday', birthday);
+    formData.append('gender', gender);
+    formData.append('job', job);
+    
     const config = {
-      Headers: {
-        "Content-Type": "multipart/form-data",
+      headers: {
+        'Content-Type': 'multipart/form-data',
       },
     };
+    
     return axios.post(url, formData, config);
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleFromSubmit}>
-        <h1>고객추가</h1>
-        프로필 이미지 :{" "}
-        <input
-          type="file"
-          name="file"
-          file={this.state.file}
-          value={this.state.fileName}
-          onChange={this.handleFileChange}
-        />
-        <br />
-        이름 :{" "}
-        <input
-          type="text"
-          name="name"
-          value={this.state.name}
-          onChange={this.handleValueChange}
-        />
-        <br />
-        생년월일 :{" "}
-        <input
-          type="text"
-          name="birthday"
-          value={this.state.birthday}
-          onChange={this.handleValueChange}
-        />
-        <br />
-        성별 :{" "}
-        <input
-          type="text"
-          name="gender"
-          value={this.state.gender}
-          onChange={this.handleValueChange}
-        />
-        <br />
-        직업 :{" "}
-        <input
-          type="text"
-          name="job"
-          value={this.state.job}
-          onChange={this.handleValueChange}
-        />
-        <br />
-        <button type="submit">추가하기</button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleFromSubmit}>
+      <h1>고객 추가</h1>
+      프로필 이미지 :{' '}
+      <input
+        type="file"
+        name="file"
+        file={file}
+        value={fileName}
+        onChange={handleFileChange}
+      />
+      <br />
+      이름 :{' '}
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleValueChange}
+      />
+      <br />
+      생년월일 :{' '}
+      <input
+        type="text"
+        name="birthday"
+        value={birthday}
+        onChange={handleValueChange}
+      />
+      <br />
+      성별 :{' '}
+      <input
+        type="text"
+        name="gender"
+        value={gender}
+        onChange={handleValueChange}
+      />
+      <br />
+      직업 :{' '}
+      <input
+        type="text"
+        name="job"
+        value={job}
+        onChange={handleValueChange}
+      />
+      <br />
+      <button type="submit">추가하기</button>
+    </form>
+  );
 }
 
 export default CustomerAdd;
