17b4dc9db30e61b9dd6ba2a34934da2ccab029cd
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2024-08-07 15:39:27 +0900

    고객추가 양식 및 이벤트 핸들링

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
+import CustomerAdd from "./components/CustomerAdd";
 import {
   Paper,
   Table,
@@ -29,7 +30,6 @@ const StyledCircularProgress = styled(CircularProgress)(({ theme }) => ({
 //실질적인 웹사이트 화면에 대한 내용 출력
 
 function App() {
-
   const [customers, setCustomers] = useState([]);
   const [completed, setCompleted] = useState(0);
   const theme = useTheme();
@@ -58,45 +58,46 @@ function App() {
   }, []);
 
   return (
-    <StyledPaper>
-      <StyledTable>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers ? (
-            customers.map((c) => (
-              <Customer
-                key={c.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            ))
-          ) : (
+    <div>
+      <StyledPaper>
+        <StyledTable>
+          <TableHead>
             <TableRow>
-              <TableCell colSpan="6" align="center">
-                <StyledCircularProgress
-                  variant="indeterminate"
-                />
-              </TableCell>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </StyledTable>
-    </StyledPaper>
+          </TableHead>
+          <TableBody>
+            {customers ? (
+              customers.map((c) => (
+                <Customer
+                  key={c.id} // map 을 사용하려면 key 라는 속성이 있어야 함(안하면 Console창에 에러가 발생)
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <StyledCircularProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </StyledTable>
+      </StyledPaper>
+      <CustomerAdd/>
+    </div>
+
   );
 }
 
 export default App;
-
---
+++ Client/src/components/CustomerAdd.js
@@ -0,0 +1,70 @@
+import React from "react";
+import axios from "axios";
+
+class CustomerAdd extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+            File: null, //바이트 현태의 데이터
+            username:'',
+            birthday:'',
+            gender:'',
+            job:'',
+            fileName:''//파일이름
+        }
+
+    }
+    handleFromSubmit=(e)=>{
+        e.preventDefault()
+        this.addCustomer()
+            .then((response)=>{
+                console.log(response.data);
+            })
+    }
+
+    handleFileChange=(e)=>{
+        this.setState({
+            file: e.target.files[0], //하나의 파일만
+            fileName: e.target.value
+        })
+    }
+
+    handleValueChange=(e)=>{
+        let nextState={};
+        nextState[e.target.name]=e.target.value;
+        this.setState(nextState);
+    }
+    
+    addCustomer = ()=>{
+        const url = '/apl/customers';
+        const formData = new FormData();
+        formData.append('image',this.state.file);
+        formData.append('username',this.state.username);
+        formData.append('birthday',this.state.birthday);
+        formData.append('gender',this.state.gender);
+        formData.append('job',this.state.job);
+        const config={
+            Headers:{
+                'content-type' : 'multipart/form=data'
+            }
+        }
+        return axios.post(url, formData, config);
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleFromSubmit}>
+                <h1>고객추가</h1>
+                프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                이름 : <input type="text" name="username" value={this.state.username} onChange={this.handleValueChange}/><br/>
+                생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
+                <button type="submit">추가하기</button>
+            </form>
+        )
+    }
+}
+
+export default CustomerAdd;
